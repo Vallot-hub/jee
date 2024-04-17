2462db5461a5937b77ecaeada2263b1c2a304eb1
Vallot-hub <59056759+Vallot-hub@users.noreply.github.com>
2024-04-17 21:59:06 +0200

    Delete src/main directory

--- src/main/java/fr/univtours/polytech/exam/dao/UserDao.java
+++
@@ -1,6 +0,0 @@
-package fr.univtours.polytech.exam.dao;
-
-
-public interface UserDao {
-    public boolean verifyPassword (String login, String password);
-} 
--- src/main/java/fr/univtours/polytech/exam/model/ResultBean.java
+++
@@ -1,6 +0,0 @@
-package fr.univtours.polytech.exam.model;
-
-public class ResultBean {
-    ArticleBean articleBean;
-    UserBean userBean;
-}
