f6abe5cb0c7f1fee4d5dc0ca0b714993bdb49a59
Vallot <christophe.vallotpro@gmail.com>
2024-04-17 12:08:09 +0200

    article

--- src/main/java/fr/univtours/polytech/exam/model/ArticleBean.java
+++ src/main/java/fr/univtours/polytech/exam/model/ArticleBean.java
@@ -1,5 +1,48 @@
 package fr.univtours.polytech.exam.model;
 
-public class ArticleBean {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity(name = "ARTICLE")
+public class ArticleBean {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Float price;
+    private Integer nbRestant;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public Integer getNbRestant() {
+        return nbRestant;
+    }
+
+    public void setNbRestant(Integer nbRestant) {
+        this.nbRestant = nbRestant;
+    }
 }
